a352059db771081f051e27c56ba5a94be3c26d6f
Ya-Lan Yang <ylyang@illinois.edu>
2024-08-12 18:53:41 -0500

    fix schema

--- defs/walkstac.py
+++ defs/walkstac.py
@@ -215,7 +215,9 @@ def validate_catalog(file_path):
 def process_catalog(catalog, base):
     try:
         # Check if the catalog has a method or attribute to get its href (URL or path)
+        print("xxx")
         catalog_url = catalog.get_self_href()  # This is just an example; adjust based on actual methods available
+        print(catalog_url)
 
         # Now open a client with the correct URL or path
         client = Client.open(catalog_url)
@@ -227,7 +229,7 @@ def process_catalog(catalog, base):
                 filepath = f"{base}/{item.target}"
                 validate_collection(filepath)
     except Exception as e:
-        print(f"    An error occurred: {e}")
+        print(f"    *An error occurred: {e}")
 
 
 def walk_catalog(root_catalog):
@@ -240,6 +242,8 @@ def walk_catalog(root_catalog):
             for l2 in l2_child_catalogs:
                 walk_catalog(child)
         elif child.STAC_OBJECT_TYPE == STACObjectType.COLLECTION:
+            print("xxx")
+            print(child.id)
             try:
                 collections = list(child.get_all_collections())
                 print(f"Number of collections: {len(collections)}")
@@ -248,7 +252,7 @@ def walk_catalog(root_catalog):
                     print(f"- {collection.id}")
                     walk_collection(root_catalog, child, collection)
             except Exception as e:
-                print(f"    An error occurred: {e}")
+                print(f"    **An error occurred: {e}")
         else:
             logging.error(f"uknonwn type")
 
@@ -292,7 +296,7 @@ def create_folder_if_not_exist(folder_path):
 
 def adjust_bbox(bbox):
     # Check if the bbox has an extra pair of square brackets
-    if isinstance(bbox, list) and len(bbox) == 1 and isinstance(bbox[0], list):
+    while isinstance(bbox, list) and len(bbox) == 1 and isinstance(bbox[0], list):
         # Flatten the list by removing the outer list
         return bbox[0]
     return bbox
@@ -349,7 +353,11 @@ def walk_stac(cf):
     replace_in_folder('./data/challenge', '"href": {}', '"href": "www.example.com"')
     replace_in_folder('./data/challenge', 'InfT00:00:00Z', '2023-10-01T00:00:00Z')
     replace_in_folder('./data/challenge', '-InfT00:00:00Z', '2024-09-05T00:00:00Z')
-    replace_in_folder('./data/challenge', '\"', '`')
+    replace_in_folder('./data/challenge', '-2023-10-01T00:00:00Z', '2023-10-01T00:00:00Z')
+    replace_in_folder('./data/challenge', r'\"', '`')
+    replace_in_folder('./data/challenge', r"\u201d", '`')
+    replace_in_folder('./data/challenge', r"\u201c", '`')
+
 
     root_catalog = Catalog.from_file(href=cf)
     ic(root_catalog)
